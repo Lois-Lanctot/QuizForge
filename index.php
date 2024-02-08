b0c1cabfc31be67daf0fca72e3b628823e477a1d
Lois Lanctot <lanctot.lois@student.greenriver.edu>
2024-02-07 23:55:45 -0500

    added create choice page and personality and trivia title page

--- index.php
+++ index.php
@@ -24,5 +24,53 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+
+$f3->route('GET /addChoice', function() {
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/add/choice.html');
+});
+
+
+$f3->route('GET|POST /addTriviaTitle', function($f3) {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $desc = $_POST['desc'];
+        $title = $_POST['title'];
+
+        $f3->set('SESSION.desc', $desc);
+        $f3->set('SESSION.title', $title);
+
+//        $f3->reroute('order2');
+    }
+
+    $view = new Template();
+    echo $view->render('views/add/trivia/trivia_title.html');
+});
+
+
+
+
+$f3->route('GET|POST /addPersonalityTitle', function($f3) {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $desc = $_POST['desc'];
+        $title = $_POST['title'];
+
+        $f3->set('SESSION.desc', $desc);
+        $f3->set('SESSION.title', $title);
+
+//        $f3->reroute('order2');
+    }
+
+    $view = new Template();
+    echo $view->render('views/add/personality/personality_title.html');
+});
+
+
+
+
+
 //Run Fat-Free
 $f3->run();
