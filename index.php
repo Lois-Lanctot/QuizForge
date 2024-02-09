33b55392bc8c489a32bf84b2405500ab8c766d00
Noah Lanctot <noah.lanctot@gmail.com>
2024-02-08 23:56:30 -0500

    Added comments for each file

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 /*
- * Lois Lanctot, Noah Lanctot
- * February 2024
- * https://lois.greenriverdev.com/328/QuizForge/
- * This is our CONTROLLER for the QuizForge app
+ * Authors: Noah Lanctot, Lois Lanctot
+ * Date: February 2024
+ * Description: This is our CONTROLLER for the QuizForge app
+ * Website: https://lois.greenriverdev.com/328/QuizForge/
  */
 
-//Turn on error reporting
+// Turn on error reporting
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-//Require the autoload file
+// Require the autoload file
 require_once ('vendor/autoload.php');
 
-//Instantiate F3
+// Instantiate F3
 $f3 = Base::instance();
 
-//Define a default route
+// Define a default route
 $f3->route('GET /', function() {
 
-    //Display a view page
+    // Display the home view page
     $view = new Template();
     echo $view->render('views/home.html');
 });
@@ -27,7 +27,7 @@ $f3->route('GET /', function() {
 
 $f3->route('GET /addChoice', function() {
 
-    //Display a view page
+    // Display the add choice view page
     $view = new Template();
     echo $view->render('views/add/choice.html');
 });
@@ -35,42 +35,38 @@ $f3->route('GET /addChoice', function() {
 
 $f3->route('GET|POST /addTriviaTitle', function($f3) {
 
+    // Handle POST request to add trivia title
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $desc = $_POST['desc'];
         $title = $_POST['title'];
 
         $f3->set('SESSION.desc', $desc);
         $f3->set('SESSION.title', $title);
-
-//        $f3->reroute('order2');
     }
 
+    // Display the add trivia title view page
     $view = new Template();
     echo $view->render('views/add/trivia/trivia_title.html');
 });
 
 
-
-
 $f3->route('GET|POST /addPersonalityTitle', function($f3) {
 
+    // Handle POST request to add personality title
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $desc = $_POST['desc'];
         $title = $_POST['title'];
 
         $f3->set('SESSION.desc', $desc);
         $f3->set('SESSION.title', $title);
-
-//        $f3->reroute('order2');
     }
 
+    // Display the add personality title view page
     $view = new Template();
     echo $view->render('views/add/personality/personality_title.html');
 });
 
 
-
-
-
-//Run Fat-Free
+// Run Fat-Free
 $f3->run();
+?>
